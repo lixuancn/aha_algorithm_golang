319b3070ffcd9171b37c3ce40a57b625610d160f
lixuancn <lixuan868686@163.com>
2017-08-17 13:22:39 +0800

    第四章第一节第二篇：深度搜索-火柴棍的数字

--- chapter-4/1-2.go
+++ chapter-4/1-2.go
@@ -1,87 +1,52 @@
-//炸弹人
-//地图是13*13，G是敌人，#是墙，.是空地
-//x,y对应坐标轴
-
+//深度优先搜索 - 火柴棍拼数字
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+//火柴总数
+var total = 48
+
+var boxCount = 9
+var a [10]int
+
+var numCount = 10
+var book [10]int
+
 
 func main(){
-	x := 13
-	y := 13
-	gameMap := [13]string{
-		"#############",
-		"#GG.GGG#GGG.#",
-		"###.#G#G#G#G#",
-		"#.......#..G#",
-		"#G#.###.#G#G#",
-		"#GG.GGG.#.GG#",
-		"#G#.#G#.#.###",
-		"##G...G.....#",
-		"#G#.#G###.#G#",
-		"#...G#GGG.GG#",
-		"#G#.#G#G#.#G#",
-		"#GG.GGG#G.GG#",
-		"#############",
-	}
-	bestX := 0
-	bestY := 0
-	bestNum := 0
-	var m, n int
-	for i:=0; i<x; i++{
-		for j:=0; j<y; j++{
-			//在空地上放炸弹
-			if gameMap[i][j] != '.'{
-				continue
-			}
-			//消灭敌人的总数
+	dfs(1)
+}
+
+func dfs(step int){
+	if step > boxCount{
+		intAndNumMap := [10]int{6, 2, 5,5,4,5,6,3,7,6}
+		if a[1] == 0 || a[4] == 0 || a[5] ==0{
+			return
+		}
+		if 100*a[1] + 10*a[2] + a[3] + 100*a[4] + 10*a[5] + a[6] == 100*a[7] + 10*a[8] + a[9]{
 			sum := 0
-			//计算左边可以消灭的敌人数量
-			m = i
-			n = j
-			for gameMap[m][n] != '#'{
-				if gameMap[m][n] == 'G'{
-					sum++
+			for k, i := range a{
+				if k == 0{
+					continue
 				}
-				m--
+				sum += intAndNumMap[i]
 			}
-			//计算右边可以消灭的敌人数量
-			m = i
-			n = j
-			for gameMap[m][n] != '#'{
-				if gameMap[m][n] == 'G'{
-					sum++
-				}
-				m++
-			}
-			//计算下边可以消灭的敌人数量
-			m = i
-			n = j
-			for gameMap[m][n] != '#'{
-				if gameMap[m][n] == 'G'{
-					sum++
-				}
-				n--
-			}
-			//计算上边可以消灭的敌人数量
-			m = i
-			n = j
-			for gameMap[m][n] != '#'{
-				if gameMap[m][n] == 'G'{
-					sum++
-				}
-				n++
-			}
-			fmt.Println(sum)
-			if sum > bestNum{
-				bestX = i
-				bestY = j
-				bestNum = sum
+			//等号和加好个需要2个火柴
+			if sum == total - 4{
+				fmt.Println(fmt.Sprintf("%d+%d=%d", 100*a[1] + 10*a[2] + a[3], 100*a[4] + 10*a[5] + a[6], 100*a[7] + 10*a[8] + a[9]))
 			}
 		}
+		return
 	}
-	msg := fmt.Sprintf("放置在%d, %d可以消灭的敌人最多， 是%d", bestX, bestY, bestNum)
-	fmt.Println(msg)
+	for i:=0; i<numCount; i++{
+		if book[i] == 1{
+			continue
+		}
+		a[step] = i
+		book[i] = 1
+		newStep := step + 1
+		dfs(newStep)
+		book[i] = 0
+	}
+	return
 }
